e26904c1a9c1011201f54a0d8eadeab33601bd6a
김범준 <kejiegohome@kakao.com>
2022-01-20 12:41:57 +0900

    Update dashboard.py
    tew

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -15,8 +15,7 @@ class AdminDashboard(Resource):
             .filter(LectureUser.user_id == Users.id)\
             .group_by(Lectures.id)\
             .all()
-
-
+        
         amount_list = [{'lecture_title' : row[0], 'amount' : int(row[1])} for row in lecture_fee_amount]
 
 
@@ -30,7 +29,7 @@ class AdminDashboard(Resource):
         diff_days = datetime.timedelta(days= -10)
         ten_days_ago = now + diff_days
 
-        amount_by_date_list = db.session.query()\
+        amount_by_date_list = db.session.query(db.func.date(LectureUser.created_at),db.func.sum(Lectures.fee))\
             .filter(Lectures.id == LectureUser.lecture_id)\
             .filter(LectureUser.created_at > ten_days_ago)\
             .group_by(db.func.date(LectureUser.created_at))\
@@ -39,16 +38,21 @@ class AdminDashboard(Resource):
         date_amounts = []
 
         for i in range(0, 11):
+
             amount_dict = {
                 'date' : ten_days_ago.strftime('%Y-%m-%d'),
                 'amount' : 0
             }
+
+            for row in amount_by_date_list:
+                if str(row[0]) == amount_dict['date']:
+                    amount_dict['amount'] = int(row[1])
             date_amounts.append(amount_dict)
-            ten_days_ago =+ datetime.timedelta(days=1)
+            ten_days_ago += datetime.timedelta(days=1)
 
         # for row in amount_by_date_list:
         #     amount_dict={
-        #         'data' : str(row[0]),
+        #         'date' : str(row[0]),
         #         'amount' : int(row[1])
         #     }
 
